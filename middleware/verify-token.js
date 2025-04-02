1fb942eb96e8ee58f337f1bdd1a7bf04e41dd3fa
Angellsworth <angellsworth@msn.com>
2025-04-02 14:32:25 -0600

    wip: added middleware

--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -1,5 +1,14 @@
 const jwt = require('jsonwebtoken')
 
 function verifyToken(req, res, next) {
-    
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decoded.payload; //adds the user object to req.user 
+        next(); //without calling next the request will stall
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid token.' })
+    }
 }
+
+module.exports = verifyToken;
